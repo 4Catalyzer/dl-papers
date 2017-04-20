d34f677890a0c3f47e6fb1be349c2d5cde2ab3ea
Jimmy Jia <tesrin@gmail.com>
2017-04-19 22:23:17 -0400

    Benchmark with constant data

--- dl_papers/wide_resnet/train.py
+++ dl_papers/wide_resnet/train.py
@@ -1,13 +1,16 @@
+from __future__ import division
+
 import functools
 import logging
+import math
 import time
 
 import click
+import numpy as np
 import tensorflow as tf
 
 from dl_papers.common.cli import cli
 from dl_papers.common.losses import global_l2_regularization_loss
-from dl_papers.common.train import run_epochs
 from dl_papers.datasets import cifar
 
 from . import models
@@ -19,6 +22,8 @@ logger = logging.getLogger(__name__)
 BATCH_SIZE = 128
 
 CIFAR_NUM_EPOCHS = 200
+CIFAR_EPOCH_SIZE = 50000
+CIFAR_NUM_BATCHES = int(math.ceil(CIFAR_EPOCH_SIZE / BATCH_SIZE))
 
 # -----------------------------------------------------------------------------
 
@@ -38,31 +43,30 @@ def get_cifar_learning_rate(i):
 
 
 def _train(
-    data,
     image_shape,
     model,
     num_epochs,
-    batch_iterator,
     get_learning_rate,
     l2_regularization_scale=5e-4,
 ):
     logger.info("building graph")
 
-    x_train, x_test, y_train, y_test = data
+    x = tf.constant(
+        np.zeros((BATCH_SIZE,) + image_shape),
+        dtype=tf.float32,
+    )
+    y_ = tf.constant(
+        np.zeros((BATCH_SIZE,)),
+        dtype=tf.int32,
+    )
 
-    x = tf.placeholder(tf.float32, shape=(None,) + image_shape)
-    y_ = tf.placeholder(tf.int32, shape=(None,))
     training = tf.placeholder_with_default(False, shape=())
     learning_rate = tf.placeholder(tf.float32, shape=())
 
     logits = model(x, training=training)
-    y = tf.to_int32(tf.argmax(logits, axis=1))
 
     loss = tf.losses.sparse_softmax_cross_entropy(
         labels=y_, logits=logits,
-    )
-    accuracy = tf.contrib.metrics.accuracy(
-        labels=y_, predictions=y,
     )
 
     regularization_loss = global_l2_regularization_loss(
@@ -75,37 +79,20 @@ def _train(
     )
     train_op = tf.contrib.training.create_train_op(total_loss, optimizer)
 
-    for sess, i, batches_train in run_epochs(
-        num_epochs,
-        get_batches=(
-            lambda: batch_iterator(x_train, y_train, training=True),
-        ),
-    ):
-        for j, (x_batch, y_batch) in enumerate(batches_train):
-            start_time = time.time()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-            (
-                _,
-                batch_loss,
-                batch_regularization_loss,
-                batch_accuracy,
-            ) = sess.run(
-                (
-                    train_op,
-                    loss,
-                    regularization_loss,
-                    accuracy,
-                ),
-                feed_dict={
-                    x: x_batch,
-                    y_: y_batch,
+        for i in range(num_epochs):
+            for j in range(CIFAR_NUM_BATCHES):
+                start_time = time.time()
+
+                sess.run(train_op, feed_dict={
                     training: True,
                     learning_rate: get_learning_rate(i),
-                },
-            )
+                })
 
-            batch_time = time.time() - start_time
-            logging.info('epoch %d, batch %d: %.3fs', i, j, batch_time)
+                batch_time = time.time() - start_time
+                logging.info('epoch %d, batch %d: %.3fs', i, j, batch_time)
 
 
 # -----------------------------------------------------------------------------
@@ -120,11 +107,9 @@ def train_cifar10(gated):
         model = models.wide_resnet_cifar10
 
     _train(
-        cifar.load_cifar10_data(),
         cifar.IMAGE_SHAPE_CHANNELS_FIRST,
         functools.partial(model, data_format='channels_first'),
         CIFAR_NUM_EPOCHS,
-        cifar.BatchIterator(BATCH_SIZE, data_format='channels_first'),
         get_cifar_learning_rate,
     )
 
